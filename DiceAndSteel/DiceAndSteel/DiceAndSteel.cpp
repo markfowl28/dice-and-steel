a2eb91b6d620706d177a77a74e8a6da6dc545990
markfowl28 <markfowl28@gmail.com>
2026-02-06 23:28:51 -0500

    Initial Console Setup

---
+++ DiceAndSteel/DiceAndSteel/DiceAndSteel.cpp
@@ -0,0 +1,9 @@
+// DiceAndSteel.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Dice & Steel starting up...\n";
+}
